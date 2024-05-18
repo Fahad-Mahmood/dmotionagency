f107dd4d0755f1f359e0e1819526698ee7f3763f
Fahad-Mahmood <fahad492@outlook.com>
2024-05-19 01:16:32 +0500

    feat: refactoring ui

--- app/(default)/page.tsx
+++ app/(default)/page.tsx
@@ -1,22 +1,14 @@
 export const metadata = {
-  title: 'Home - Open PRO',
-  description: 'Page description',
+  title: 'Home - DMotion Agency',
+  description: 'Amazing Motion Graphics and Story-telling videos for the best',
 }
 
 import Hero from '@/components/hero'
-import Features from '@/components/features'
-import Newsletter from '@/components/newsletter'
-import Zigzag from '@/components/zigzag'
-import Testimonials from '@/components/testimonials'
 
 export default function Home() {
   return (
     <>
       <Hero />
-      <Features />
-      <Zigzag />
-      <Testimonials />
-      <Newsletter />
     </>
   )
 }
--- components/hero.tsx
+++ components/hero.tsx
@@ -1,4 +1,4 @@
-import VideoThumb from '@/public/images/hero-image-01.jpg'
+import VideoThumb from '@/public/images/hero-image-01.png'
 import ModalVideo from '@/components/modal-video'
 
 export default function Hero() {
@@ -24,18 +24,9 @@ export default function Hero() {
 
           {/* Section header */}
           <div className="max-w-3xl mx-auto text-center pb-12 md:pb-16">
-            <h1 className="h1 mb-4" data-aos="fade-up">Landing template for startups</h1>
-            <p className="text-xl text-gray-400 mb-8" data-aos="fade-up" data-aos-delay="200">Our landing page template works on all devices, so you only have to set it up once, and get beautiful results forever.</p>
-            <div className="max-w-xs mx-auto sm:max-w-none sm:flex sm:justify-center">
-              <div data-aos="fade-up" data-aos-delay="400">
-                <a className="btn text-white bg-purple-600 hover:bg-purple-700 w-full mb-4 sm:w-auto sm:mb-0" href="#0">Start free trial</a>
-              </div>
-              <div data-aos="fade-up" data-aos-delay="600">
-                <a className="btn text-white bg-gray-700 hover:bg-gray-800 w-full sm:w-auto sm:ml-4" href="#0">Learn more</a>
-              </div>
-            </div>
+            <h1 className="h1 mb-4" data-aos="fade-up">Our Brains Processes Visuals 600x Better</h1>
+            <p className="text-xl text-gray-400 mb-8" data-aos="fade-up" data-aos-delay="200">It's time to engage your customers with interactive visuals on the landing pages.</p>
           </div>
-
           <ModalVideo
             thumb={VideoThumb}
             thumbWidth={1024}
@@ -44,9 +35,7 @@ export default function Hero() {
             video="/videos/video.mp4"
             videoWidth={1920}
             videoHeight={1080} />
-
         </div>
-
       </div>
     </section>
   )
